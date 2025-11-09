cc139e21b0da094b31b66a0228a3c482ee3d3378
Felipe Haddad <165858976+FelipeHaddad@users.noreply.github.com>
2025-11-09 22:08:17 +0000

    Estrutura inicial do projeto + HashTable e Node

---
+++ src/HashTable/Node.java
@@ -0,0 +1,27 @@
+package HashTable;
+
+public class Node {
+    private String palavra;
+    private int frequencia;
+    private Node proximo;
+
+    public Node(String palavra) {
+        this.palavra = palavra;
+        this.frequencia = 1;
+        this.proximo = null;
+    }
+
+    public void setPalavra(String palavra) { this.palavra = palavra; }
+
+    public void setFrequencia(int frequencia) { this.frequencia = frequencia; }
+
+    public void setProximo(Node proximo) { this.proximo = proximo; }
+
+    public String getPalavra() { return this.palavra; }
+
+    public int getFrequencia() { return this.frequencia; }
+
+    public Node getProximo() { return this.proximo; }
+
+    public void incrementarFrequencia() { this.frequencia++; }
+}
